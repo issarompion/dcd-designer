4277eb5ac72f8b046bea34815a21f1e125fd74df
issarompion <irompion@yahoo.fr>
2019-08-05 15:59:10 +0200

    delete dcd-ui-angular

--- projects/dcd-ui-angular/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of dcd-ui-angular
- */
-
-export * from './lib/dcd-ui-angular.service';
-export * from './lib/dcd-ui-angular.component';
-export * from './lib/dcd-ui-angular.module';
